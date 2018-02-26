33a74b7094fc8f5af23ff38d3100ca4d58af19c7
Slim HAMMAMI <slim.hammami.ext@easytek.tn>
2018-02-26 22:00:26 +0100

    Add project files.

---
+++ TEKUP_Trainning/TEKUP_Trainning/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TEKUP_Trainning
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            InitializeComponent();
+
+
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Alert ! ", "Hello from TEK-UP", "Cancel");
+
+        }
+
+        //private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        // //   MyLabel.Text = String.Format("Value is {0}",e.NewValue);
+        //}
+    }
+}
